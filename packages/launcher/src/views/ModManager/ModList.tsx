bce64a42b747b2ba94518be6c87594361d15bf6c
median-dxz <med_iridescence@163.com>
2024-03-29 23:44:46 +0800

    feat & ui: 添加mod管理相关组件

--- packages/launcher/src/views/ModManager/ModList.tsx
+++ packages/launcher/src/views/ModManager/ModList.tsx
@@ -1,13 +1,34 @@
+import { List, ListItem, ListItemText, Menu, MenuItem, Typography, alpha, type ListProps } from '@mui/material';
+import React, { useState, type MouseEventHandler } from 'react';
+
+import { IconButtonNoRipple } from '@/components/IconButtonNoRipple';
 import { useModStore } from '@/context/useModStore';
 import type { ModInstance } from '@/service/store/mod';
-import { List, ListItemButton, ListItemText, Typography, alpha, type ListProps } from '@mui/material';
-import React from 'react';
+
+import Settings from '@mui/icons-material/Settings';
+import Refresh from '@mui/icons-material/RefreshRounded';
+import Delete from '@mui/icons-material/DeleteOutlineRounded';
+import RadioButtonUnchecked from '@mui/icons-material/RadioButtonUnchecked';
+import RadioButtonChecked from '@mui/icons-material/RadioButtonChecked';
 
 interface ModListItemProps {
     mod: ModInstance;
 }
 
 export function ModListItem({ mod }: ModListItemProps) {
+    const [anchor, setAnchor] = useState<HTMLElement | null>(null);
+
+    const handleClick: MouseEventHandler<HTMLButtonElement> = (e) => {
+        const target = e.currentTarget;
+        setAnchor(target);
+    };
+
+    const handleCloseMenu = React.useCallback(() => {
+        setAnchor(null);
+    }, [setAnchor]);
+
+    const open = Boolean(anchor);
+
     const { meta } = mod;
     const title = (
         <>
@@ -20,9 +41,14 @@ export function ModListItem({ mod }: ModListItemProps) {
 
     const description = (
         <>
-            <Typography component="span" fontSize={16}>
+            <Typography component="span" fontSize={16} sx={{ paddingRight: '1em' }}>
                 {meta.namespace} {meta.description}
             </Typography>
+            {meta.preload && (
+                <Typography component="span" color="GrayText" fontSize={16}>
+                    预加载
+                </Typography>
+            )}
             <Typography component="span" color="GrayText" sx={{ float: 'right' }} fontSize={16}>
                 {mod.strategy.length ? `策略: ${mod.strategy.length}` : ''}
             </Typography>
@@ -38,8 +64,20 @@ export function ModListItem({ mod }: ModListItemProps) {
         </>
     );
     return (
-        <ListItemButton
-            sx={{ height: '72px', border: (theme) => `1px solid ${alpha(theme.palette.primary.main, 0.12)}` }}
+        <ListItem
+            sx={{
+                transition: (theme) => theme.transitions.create(['background-color']),
+                borderRadius: '6px',
+                border: (theme) => `1px solid ${alpha(theme.palette.primary.main, 0.12)}`,
+                '&:hover': {
+                    backgroundColor: (theme) => {
+                        // if (error) {
+                        //     return alpha(theme.palette.error.main, 0.18);
+                        // }
+                        return alpha(theme.palette.primary.main, 0.12);
+                    }
+                }
+            }}
             onClick={() => {
                 console.log(mod);
                 //TODO open mod detail
@@ -49,8 +87,49 @@ export function ModListItem({ mod }: ModListItemProps) {
                 primary={title}
                 secondary={description}
                 primaryTypographyProps={{ fontFamily: ['Open Sans', 'MFShangHei'] }}
+                sx={{ paddingRight: 4, paddingLeft: 4 }}
             />
-        </ListItemButton>
+            <IconButtonNoRipple title="设置" sx={{ marginInline: '2rem' }} onClick={handleClick}>
+                <Settings />
+            </IconButtonNoRipple>
+            <Menu
+                anchorEl={anchor}
+                MenuListProps={{
+                    sx: {
+                        maxHeight: '50vh',
+                        overflowY: 'auto',
+                        bgcolor: (theme) => alpha(theme.palette.secondary.main, 0.88)
+                    }
+                }}
+                open={open}
+                onClose={handleCloseMenu}
+            >
+                <MenuItem sx={{ maxWidth: '25vw', fontSize: '1rem' }}>
+                    <RadioButtonUnchecked fontSize="inherit" />
+                    <Typography sx={{ ml: 2 }} fontSize="inherit">
+                        启用
+                    </Typography>
+                </MenuItem>
+                <MenuItem sx={{ maxWidth: '25vw', fontSize: '1rem' }}>
+                    <RadioButtonChecked fontSize="inherit" />
+                    <Typography sx={{ ml: 2 }} fontSize="inherit">
+                        禁用
+                    </Typography>
+                </MenuItem>
+                <MenuItem sx={{ maxWidth: '25vw', fontSize: '1rem' }}>
+                    <Refresh fontSize="inherit" />
+                    <Typography sx={{ ml: 2 }} fontSize="inherit">
+                        重载
+                    </Typography>
+                </MenuItem>
+                <MenuItem sx={{ maxWidth: '25vw', fontSize: '1rem' }}>
+                    <Delete fontSize="inherit" />
+                    <Typography sx={{ ml: 2 }} fontSize="inherit">
+                        删除
+                    </Typography>
+                </MenuItem>
+            </Menu>
+        </ListItem>
     );
 }
 
@@ -59,11 +138,7 @@ export function ModList(listProps: ListProps) {
     const store = Array.from(_store.values());
     return (
         <List
-            sx={{
-                width: '100%',
-                paddingTop: '16px',
-                paddingLeft: '16px'
-            }}
+            sx={{ width: '100%', overflow: 'auto', paddingRight: 1, '& > *:not(:first-child)': { marginTop: '8px' } }}
             {...listProps}
         >
             {store.map((mod) => {
