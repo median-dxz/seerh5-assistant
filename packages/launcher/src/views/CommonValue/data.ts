e03e0cd253eb32e1912e54cc9ec3620eda7c4558
median-dxz <med_iridescence@163.com>
2024-09-19 21:50:06 +0800

    perf: 添加更多的物品查询支持 & core中不再提供itemId

--- packages/launcher/src/views/CommonValue/data.ts
+++ packages/launcher/src/views/CommonValue/data.ts
@@ -1,25 +1,30 @@
 import { delay, engine, type AnyFunction } from '@sea/core';
 
 export const idList = [
-    1, //赛尔豆
-    300065, //特性重组剂Ω
-    300066, //通用刻印激活水晶
-    1400154, //友谊之星
-    1400352, //泰坦之灵
-    1707511, //功勋点数
-    // 记忆模组
-    // 激励模组
-    1721558, //界神印记
-    1721761, //王者精魄
-    1706927, //红宝石
-    1706928, //绿宝石
-    1717452, //荣耀铸币
-    1717451, //圣战奖章
-    // 全面药
-    // 龙药
-    // 不灭珠
-    1722506 //先锋旗帜
-    // spt奖章
+    1, // 赛尔豆
+    1400154, // 友谊之星
+    300066, // 通用刻印激活水晶
+    300065, // 特性重组剂Ω
+    300697, // 全面药
+    300810, // 龙药
+    300785, // 不灭珠
+    1400352, // 泰坦之灵
+    1706927, // 红宝石
+    1706928, // 绿宝石
+    1400153, // 记忆模组
+    2500005, // 记忆模组限时
+    1400152, // 激励模组
+    1723255, // 浩瀚星核
+    2500004, // 浩瀚星核限时
+    1723167, // 星际密令
+    2500003, // 星际密令限时
+    1707511, // 功勋点数
+    1721558, // 界神印记
+    1721761, // 王者精魄
+    1717452, // 荣耀铸币
+    1717451, // 圣战奖章
+    1722506, // 先锋旗帜
+    1725115 // spt奖章
 ];
 
 declare namespace pveEnterPanel {
@@ -53,20 +58,35 @@ export const openModuleList: Record<number, AnyFunction> = {
     },
     1707511: async () => {
         await showPveResourcePanel('ITEM');
-    }, //功勋点数
+    }, // 功勋点数
     1721558: async () => {
         await showPveResourcePanel('ITEM');
-    }, //界神印记
+    }, // 界神印记
     1721761: async () => {
         await showPveResourcePanel('MARK');
-    }, //王者精魄
+    }, // 王者精魄
     1717452: async () => {
         await ModuleManager.showModuleByID(255);
-    }, //荣耀铸币
+    }, // 荣耀铸币
     1717451: async () => {
         await ModuleManager.showModuleByID(255);
-    }, //圣战奖章
+    }, // 圣战奖章
+    1723255: async () => {
+        await ModuleManager.showModuleByID(148);
+    }, // 浩瀚星核
+    2500004: async () => {
+        await ModuleManager.showModuleByID(148);
+    }, // 浩瀚星核限时
+    1723167: async () => {
+        await ModuleManager.showModuleByID(144);
+    }, // 星际密令
+    2500003: async () => {
+        await ModuleManager.showModuleByID(144);
+    }, // 星际密令限时
     1722506: async () => {
         await ModuleManager.showModule('pveSpt');
-    } //先锋旗帜
+    }, // 先锋旗帜
+    1725115: async () => {
+        await ModuleManager.showModule('pveSpt');
+    } // spt奖章
 };
