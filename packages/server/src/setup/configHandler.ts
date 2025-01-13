2aab0999d9f6640dc2871d356422b17ae61a592f
median-dxz <33348142+median-dxz@users.noreply.github.com>
2025-01-13 22:01:03 +0800

    chore: 更改任务选项的配置文件名

--- packages/server/src/setup/configHandler.ts
+++ packages/server/src/setup/configHandler.ts
@@ -10,18 +10,18 @@ import { TaskOptions } from '../configHandlers/TaskOptions.ts';
 const PET_CACHE_FILENAME = 'sea-pets.json';
 const MOD_LIST_FILENAME = 'sea-mods.json';
 const LAUNCHER_CONFIG_FILENAME = 'sea-launcher.json';
-const TASK_CONFIG_FILENAME = 'sea-task-config.json';
+const TASK_OPTIONS_FILENAME = 'sea-task-options.json';
 
 export const setupConfigHandlers = async (appRoot: string) => {
     const petCatchTimeStorage = new FileSystemStorage(path.join(appRoot, configsRoot, PET_CACHE_FILENAME));
     const launcherConfigStorage = new FileSystemStorage(path.join(appRoot, configsRoot, LAUNCHER_CONFIG_FILENAME));
     const modIndexStorage = new FileSystemStorage(path.join(appRoot, configsRoot, MOD_LIST_FILENAME));
-    const taskConfigStorage = new FileSystemStorage(path.join(appRoot, configsRoot, TASK_CONFIG_FILENAME));
+    const taskOptionsStorage = new FileSystemStorage(path.join(appRoot, configsRoot, TASK_OPTIONS_FILENAME));
 
     const petCatchTime = new PetCatchTime(petCatchTimeStorage);
     const launcherConfig = new LauncherConfig(launcherConfigStorage);
     const modIndex = new ModIndex(modIndexStorage);
-    const taskConfig = new TaskOptions(taskConfigStorage);
+    const taskConfig = new TaskOptions(taskOptionsStorage);
     await Promise.all([petCatchTime.load(), launcherConfig.load(), taskConfig.load(), modIndex.load()]);
 
     return {
