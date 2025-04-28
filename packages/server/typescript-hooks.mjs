fb18cdf362e22c355a1fc1d7e9add2090c3985b0
median-dxz <33348142+median-dxz@users.noreply.github.com>
2025-04-28 20:05:34 +0800

    perf & toolchain: 将ts运行时切换为tsx

--- packages/server/typescript-hooks.mjs
+++
@@ -1,7 +0,0 @@
-import { register } from 'node:module';
-import { setUncaughtExceptionCaptureCallback } from 'node:process';
-import { pathToFileURL } from 'node:url';
-setUncaughtExceptionCaptureCallback((err) => {
-    console.error(err);
-});
-register('ts-node/esm', pathToFileURL('./'));
